9c20bc01316d09988bc5cc3ca1f20c2825d8ae7d
ztangaj <ztangaj@connect.ust.hk>
2021-03-14 12:37:21 +0800

    init

--- grpc/cpp/code_simplification_manu/test.cpp
+++ grpc/cpp/code_simplification_manu/test.cpp
@@ -3,7 +3,7 @@ using namespace simplify;
 
 int main(int argc, char* argv[])
 {
-    std::string offpath = "/mnt/c/Users/huaxuan/source/repos/terrain_web/node/threejs/src/models/off/small_terrain.off.graph";
+    std::string offpath = "//home/tongcheng/Documents/terrain_toolkit_web/node/threejs/src/models/off/small_terrain.off.graph";
     float beta = 1.8;
     std::string outpath = "test4.off";
     generateOff(offpath, beta, outpath);
